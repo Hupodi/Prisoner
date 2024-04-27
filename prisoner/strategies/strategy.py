423f31c124d0d9896c8f7aa2e0a040cf55d76e1e
laurent <laurent@gamercraft.com>
2024-04-27 20:36:53 +0200

    Base strategy structure

--- prisoner/strategies/strategy.py
+++ prisoner/strategies/strategy.py
@@ -1,8 +1,14 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Strategy(ABC):
     """
     Abstract base class representing a strategy
     """
-    pass
+
+    @abstractmethod
+    def decide_step(self) -> bool:
+        """
+        Get the next step decision for the strategy.
+        """
+        pass
